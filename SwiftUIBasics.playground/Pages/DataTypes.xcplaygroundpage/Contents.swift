336b2624d9adfae45fb268af9782ed53e302ed2c
Alin RADU <alin.radu.cristian@gmail.com>
2024-01-12 18:26:43 +0200

    add array data type

---
+++ SwiftUIBasics.playground/Pages/DataTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+
+import Cocoa
+
+// Arrays
+
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+var albums = Array<String>()
+var albums1 = [String]()
+var albums2:[String] = []
+
