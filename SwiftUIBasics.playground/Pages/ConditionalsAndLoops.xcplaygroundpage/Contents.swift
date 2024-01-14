0cdb4720b4180079a834adba738cea08074ed0c9
Alin RADU <alin.radu.cristian@gmail.com>
2024-01-14 18:47:28 +0200

    add conditionals and loops

--- SwiftUIBasics.playground/Pages/ConditionalsAndLoops.xcplaygroundpage/Contents.swift
+++ SwiftUIBasics.playground/Pages/ConditionalsAndLoops.xcplaygroundpage/Contents.swift
@@ -135,3 +135,77 @@ let theme = Theme.dark
 
 let background = theme == .dark ? "black" : "white"
 print(background)
+
+// for loops
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for platform in platforms {
+    print("Swift works great on \(platform).")
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+// while loop
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)…")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+// create an integer to store our roll
+var roll = 0
+
+// carry on looping until we reach 20
+while roll != 20 {
+    // roll a new dice and print what it was
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+// if we're here it means the loop ended – we got a 20!
+print("Critical hit!")
+
+// skip loop iteration
+
+let filenames = ["me.jpg", "work.txt", "sophie.jpg", "logo.psd"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+
+    print("Found picture: \(filename)")
+}
+
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+
+print(multiples)
